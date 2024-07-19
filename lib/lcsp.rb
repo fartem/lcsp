d8333eb12d895a1db518f6e7c0f807301804aa35
fartem <jaman.smlnsk@gmail.com>
2024-07-19 06:58:56 +0300

    2024-07-19 v. 1.1.0: fixed checks

--- lib/lcsp.rb
+++ lib/lcsp.rb
@@ -8,7 +8,8 @@ require_relative './lcsp/resolver'
 module LCSP
   # This class represents the main functionality of the LCSP (Longest Common Subsequence Problem) tool.
   class LCSP
-    # Starts the LCSP tool by resolving the longest common subsequence for a given user, repository, and pull request number.
+    # Starts the LCSP tool by resolving the longest common subsequence for a given user, repository,
+    # and pull request number.
     #
     # @param user [String] The GitHub username of the repository owner.
     # @param repository [String] The name of the GitHub repository.
